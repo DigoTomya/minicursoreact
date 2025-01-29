fe951950db6b9d20483409824ba0d6bb6798d0ae
DigoTomya <digotm08@gmail.com>
2025-01-29 18:58:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,30 @@ import './App.css'
 import Botao from './componenetes/Botao'
 import Header from './componenetes/Header'
 
+import { useState } from 'react'
+
 function App() {
   let lista = ["Futebol", "Ginastica", "skate", "Hipismo", "Esgrima"]
   let variavel = ""
+  const [valor, setValor] = useState("");
+
+  const handleChange = (event) => {
+    setValor(event.target.value);
+  }
 
   return (
     <div>
       <p> Qual o seu nome? </p>
       <p> {variavel} </p>
       <Header></Header>
-      <Botao></Botao>
+      <p>Digite uma mensagem     
+        <input type="text" onChange={handleChange} />
+      </p>
+
+      <p>Seu texto: {valor}</p>
       
-      {
-        lista.map((elementoDaVez) => {
-          return(
-            <h2>{elementoDaVez}</h2>
-          )
-        })
-      }
+      
+    
     </div>
   )
 }
